5d61a4ab1c58d6196057a05976ab85c27ce43b49
Vlastimil Holer <holer@ics.muni.cz>
2015-07-29 17:52:06 +0200

    New /proc/stat parser

---
+++ ZenPacks/CERIT_SC/LinuxMonitorAdvanced/parsers/linux/stat.py
@@ -0,0 +1,32 @@
+from Products.ZenRRD.CommandParser import CommandParser
+
+class stat(CommandParser):
+    def processResults(self, cmd, result):
+        if cmd.result.output:
+            valueMap = dict()
+
+            for l in cmd.result.output.splitlines():
+                e = l.split()
+                if len(e) > 1:
+                    if e[0] == 'cpu':
+                        ids = ['ssCpuUser', 'ssCpuNice', 'ssCpuSystem',
+                               'ssCpuIdle', 'ssCpuWait', 'ssCpuInterrupt',
+                               'ssCpuSoftInterrupt', 'ssCpuSteal',
+                               'ssCpuGuest', 'ssCpuGuestNice']
+                        valueMap.update(dict(zip(ids, e[1:])))
+                    elif e[0] == 'ctxt':
+                        valueMap['ssCtxt'] = e[1]
+                    elif e[0] == 'processes':
+                        valueMap['ssProcsNew'] = e[1]
+                    elif e[0] == 'procs_running':
+                        valueMap['ssProcsRunning'] = e[1]
+                    elif e[0] == 'procs_blocked':
+                        valueMap['ssProcsBlocked'] = e[1]
+
+            # process values
+            datapointMap = dict([(dp.id, dp) for dp in cmd.points])
+            for id in valueMap:
+                if datapointMap.has_key(id):
+                    result.values.append((datapointMap[id], long(valueMap[id])))
+
+        return result
