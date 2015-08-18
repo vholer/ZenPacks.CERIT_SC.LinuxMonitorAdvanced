8926cf4bca4d19b3ae90deac26705a40dd088952
Vlastimil Holer <holer@ics.muni.cz>
2015-08-18 18:13:34 +0200

    Model OS vendor and full release description

--- ZenPacks/CERIT_SC/LinuxMonitorAdvanced/modeler/plugins/CERIT_SC/cmd/linux/lsb_release.py
+++ ZenPacks/CERIT_SC/LinuxMonitorAdvanced/modeler/plugins/CERIT_SC/cmd/linux/lsb_release.py
@@ -1,9 +1,11 @@
+import re
 from Products.DataCollector.plugins.CollectorPlugin import CommandPlugin
+from Products.DataCollector.plugins.DataMaps import MultiArgs
 
 class lsb_release(CommandPlugin):
     maptype = "DeviceMap"
     compname = ""
-    command = 'lsb_release -sir'
+    command = 'lsb_release -id'
     oses = ['Linux']
 
     def condition(self, device, log):
@@ -11,7 +13,23 @@ class lsb_release(CommandPlugin):
 
     def process(self, device, results, log):
         log.info("Processing the LSB release info for device %s" % device.id)
+        vendor, model = ('Unknown', 'Unknown') 
+
+        for line in results.splitlines():
+            k,v = line.split(':', 1)
+            v = v.strip()
+
+            if k == 'Distributor ID':
+                if re.search('^Red\s*Hat', v, re.IGNORECASE):
+                    vendor = 'RedHat'
+                elif re.search('^SUSE', v, re.IGNORECASE):
+                    vendor = 'SUSE'
+                else:
+                    vendor = v
+            elif k == 'Description':
+                model = v
+
         om = self.objectMap()
-        om.setOSProductKey = ' '.join(results.strip().split())
+        om.setOSProductKey = MultiArgs(model, vendor)
         log.debug("setOSProductKey=%s" % (om.setOSProductKey))
         return om
