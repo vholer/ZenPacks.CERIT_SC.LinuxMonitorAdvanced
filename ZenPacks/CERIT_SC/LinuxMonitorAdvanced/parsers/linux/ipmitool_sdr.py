7a66a3e92ba6c8914332fe974524655bc0a91494
Vlastimil Holer <holer@ics.muni.cz>
2015-09-14 10:00:23 +0200

    ipmitool sdr (+CSV) parsers

--- ZenPacks/CERIT_SC/LinuxMonitorAdvanced/parsers/linux/ipmitool_sdr.py
+++ ZenPacks/CERIT_SC/LinuxMonitorAdvanced/parsers/linux/ipmitool_sdr.py
@@ -2,9 +2,9 @@ from Products.ZenRRD.ComponentCommandParser import ComponentCommandParser
 
 class ipmitool_sdr(ComponentCommandParser):
     componentSplit = '\n'
-    componentScanValue = 'id'
-    componentScanner = '^(?P<component>[^,]+),'
+    componentScanValue = 'snmpindex'
+    componentScanner = '^[^|]+\|\s*0*(?P<component>[0-9a-fA-F]+)h\s*\|'
     scanners = [
-        r'(?P<celsius>\d+),degrees C,(?P<status>.*)$',
-        r'(?P<rpm>\d+),RPM,(?P<status>.*)$',
+        r'\|\s*(?P<celsius>\d+) degrees C\s*$',
+        r'\|\s*(?P<rpm>\d+) RPM\s*$',
     ]
---
+++ ZenPacks/CERIT_SC/LinuxMonitorAdvanced/parsers/linux/ipmitool_sdr_csv.py
@@ -0,0 +1,10 @@
+from Products.ZenRRD.ComponentCommandParser import ComponentCommandParser
+
+class ipmitool_sdr_csv(ComponentCommandParser):
+    componentSplit = '\n'
+    componentScanValue = 'id'
+    componentScanner = '^(?P<component>[^,]+),'
+    scanners = [
+        r'(?P<celsius>\d+),degrees C,(?P<state>.*)$',
+        r'(?P<rpm>\d+),RPM,(?P<state>.*)$',
+    ]
